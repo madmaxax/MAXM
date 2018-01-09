964915ec9c59801647534582d65973012c307edc
Max <maximilian.muza@gmx.de>
2018-01-09 18:49:06 +0100

    first commit

---
+++ MAXM/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  MAXM
+//
+//  Created by Max on 09.01.18.
+//  Copyright © 2018 Max. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ MAXM/HorizontalScrollBar.swift
@@ -0,0 +1,9 @@
+//
+//  HorizontalScrollBar.swift
+//  MAXM
+//
+//  Created by Max on 09.01.18.
+//  Copyright © 2018 Max. All rights reserved.
+//
+
+import Foundation
